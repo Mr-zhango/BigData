e8015ae563d590184cb366c22909df7d90b7a715
zhangyang <969666310@qq.com>
2019-11-13 18:51:47 +0800

    读取离线计算数据

---
+++ SparkStudy/src/main/scala/cn/myfreecloud/spark/hbase/Hbase03_ReadOutLineData.scala
@@ -0,0 +1,59 @@
+package cn.myfreecloud.spark.hbase
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.client.Result
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+
+/**
+ * spark读取Hbase数据
+ */
+object Hbase03_ReadOutLineData {
+
+  def main(args: Array[String]): Unit = {
+
+
+    val sparkConfig: SparkConf = new SparkConf().setMaster("local[*]").setAppName("Hbase03_ReadOutLineData")
+
+    // 创建spark上下文对象
+    val sc = new SparkContext(sparkConfig)
+
+    //构建HBase配置信息
+    val conf: Configuration = HBaseConfiguration.create()
+    conf.set("hbase.zookeeper.quorum", "hadoop102,hadoop103,hadoop104")
+
+    // 读取离线计算数据
+    conf.set(TableInputFormat.INPUT_TABLE, "event_logs20191106")
+
+
+    //从HBase读取数据形成RDD
+    val hbaseRDD: RDD[(ImmutableBytesWritable, Result)] = sc.newAPIHadoopRDD(
+      conf,
+      classOf[TableInputFormat],
+      classOf[ImmutableBytesWritable],
+      classOf[Result])
+
+    val count: Long = hbaseRDD.count()
+    println(count)
+
+    //对hbaseRDD进行处理
+    hbaseRDD.foreach {
+      case (_, result) =>
+        val key: String = Bytes.toString(result.getRow)
+        val name: String = Bytes.toString(result.getValue(Bytes.toBytes("info"), Bytes.toBytes("name")))
+        val color: String = Bytes.toString(result.getValue(Bytes.toBytes("info"), Bytes.toBytes("none")))
+        println("RowKey:" + key + ",name:" + name + ",none:" + color)
+    }
+
+    //关闭连接
+    sc.stop()
+  }
+
+
+
+}
