c17ecf9064d6c23021d964b29e6bf73c8c56369b
zhangyang <969666310@qq.com>
2019-11-13 16:42:06 +0800

    检查点

--- SparkStudy/src/main/scala/cn/myfreecloud/spark/lineage/Lineage01_CheckPoint.scala
+++ SparkStudy/src/main/scala/cn/myfreecloud/spark/lineage/Lineage01_CheckPoint.scala
@@ -14,14 +14,32 @@ object Lineage01_CheckPoint {
 
     val sparkConfig: SparkConf = new SparkConf().setMaster("local[*]").setAppName("Lineage01_CheckPoint")
 
+    // 创建spark上下文对象
     val sc = new SparkContext(sparkConfig)
 
-    val rdd: RDD[Int] = sc.makeRDD(List(1,2,3,4))
+    // 设定检查点的保存目录
 
-    val mapRDD: RDD[(Int,Int)] = rdd.map((_,1))
+    sc.setCheckpointDir("checkpointDir")
 
-    val value:RDD[(Int,Int)] = mapRDD.reduceByKey(_+_)
+    val rdd: RDD[Int] = sc.makeRDD(List(1, 2, 3, 4))
 
+    val mapRDD: RDD[(Int, Int)] = rdd.map((_, 1))
+
+    // 创建检查点
+    // val unit:Unit = mapRDD.checkpoint()
+
+    val reduceRDD: RDD[(Int, Int)] = mapRDD.reduceByKey(_ + _)
+
+    reduceRDD.checkpoint()
+
+    // 必须有行动算子才会执行checkpoint操作
+    reduceRDD.foreach(println)
+
+    // 查看血缘关系
+    println(reduceRDD.toDebugString)
+
+    // 释放资源
+    sc.stop()
 
   }
 
