b6a37075d3e5836dce77c6301249735cbed6ab08
zhangyang <969666310@qq.com>
2019-11-25 14:59:28 +0800

    代码查询kylin

--- Zepplin-Kylin/src/main/java/cn/myfreecloud/kylin/TestKylin.java
+++ Zepplin-Kylin/src/main/java/cn/myfreecloud/kylin/TestKylin.java
@@ -10,7 +10,7 @@ public class TestKylin {
         String KYLIN_DRIVER = "org.apache.kylin.jdbc.Driver";
 
         //Kylin_URL
-        String KYLIN_URL = "jdbc:kylin://hadoop102:7070/FirstProject";
+        String KYLIN_URL = "jdbc:kylin://hadoop102:7070/emp_sallary";
 
         //Kylin的用户名
         String KYLIN_USER = "ADMIN";
