09a3b50d85e34cd5e10c1515c3a9bfbc9697e291
zhangyang <969666310@qq.com>
2020-01-02 15:21:53 +0800

    kafka模式,方便测试

---
+++ UserBehaviorAnalysis/HotItemsAnalysis/src/main/scala/KafkaProducer.scala
@@ -0,0 +1,25 @@
+import java.util.Properties
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+object KafkaProducer {
+
+  def main(args: Array[String]): Unit = {
+    writeToKafka("hotitems")
+  }
+
+  def writeToKafka(topic: String): Unit = {
+    // 基本配置
+    val props = new Properties()
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    val producer = new KafkaProducer[String, String](props)
+    // 从文件中获取数据源
+    val bufferedSource = io.Source.fromFile("C:\\develop\\IDEAProjects\\BigData\\UserBehaviorAnalysis\\HotItemsAnalysis\\src\\main\\resources\\UserBehavior.csv")
+    for (line <- bufferedSource.getLines) {
+      val record = new ProducerRecord[String, String](topic, line)
+      producer.send(record)
+    }
+    producer.close()
+  }
+}
