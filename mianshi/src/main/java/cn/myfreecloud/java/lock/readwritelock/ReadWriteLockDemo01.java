98cca9d44e684acd6544b1a6f2a8e5e8724d5fb8
zhangyang <969666310@qq.com>
2020-01-09 14:47:38 +0800

    使用读写锁保证hashmap支持并发操作

--- mianshi/src/main/java/cn/myfreecloud/java/lock/readwritelock/ReadWriteLockDemo01.java
+++ mianshi/src/main/java/cn/myfreecloud/java/lock/readwritelock/ReadWriteLockDemo01.java
@@ -4,11 +4,16 @@ package cn.myfreecloud.java.lock.readwritelock;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 // 资源类
 class MyCache {
 
+    // 放入缓存的东西一般使用 volatile 修饰 需要保证可见性 (不保证原子性,禁止指令重排)
     private volatile Map<String,Object> map = new HashMap<>();
+
+    // 可重入的读写锁
+    private ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
 
     /**
      * 写操作,必须 原子+独占 ,整个过程必须是一个完整的统一体,中间不允许被分割,被打断
@@ -17,21 +22,42 @@ class MyCache {
      */
     public void put(String key,Object value){
 
-        System.out.println(Thread.currentThread().getName() + "\t 正在写入:" +key);
+        rwLock.writeLock().lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "\t 正在写入:" +key);
 
-        // 暂停0.3s 用来模拟网络拥堵的效果
-        try {TimeUnit.MICROSECONDS.sleep(300);} catch (Exception e) {e.printStackTrace();}
+            // 暂停0.3s 用来模拟网络拥堵的效果
+            try {TimeUnit.MICROSECONDS.sleep(300);} catch (Exception e) {e.printStackTrace();}
 
-        map.put(key,value);
-        System.out.println(Thread.currentThread().getName() + "\t 写入完成:");
+            map.put(key,value);
+            System.out.println(Thread.currentThread().getName() + "\t 写入完成:");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            rwLock.writeLock().unlock();
+        }
     }
 
     public void get(String key){
-        System.out.println(Thread.currentThread().getName() + "\t 正在读取:" +key);
-        // 暂停0.3s 用来模拟网络拥堵的效果
-        try {TimeUnit.MICROSECONDS.sleep(300);} catch (Exception e) {e.printStackTrace();}
-        Object result = map.get(key);
-        System.out.println(Thread.currentThread().getName() + "\t 读取完成:" + result);
+
+        rwLock.readLock().lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "\t 正在读取:");
+            // 暂停0.3s 用来模拟网络拥堵的效果
+            try {TimeUnit.MICROSECONDS.sleep(300);} catch (Exception e) {e.printStackTrace();}
+            Object result = map.get(key);
+            System.out.println(Thread.currentThread().getName() + "\t 读取完成:" + result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            rwLock.readLock().unlock();
+        }
+    }
+
+    // 清除缓存
+    public void clearMap(){
+
     }
 }
 
