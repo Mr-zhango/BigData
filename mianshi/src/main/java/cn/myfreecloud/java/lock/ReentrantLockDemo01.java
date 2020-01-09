b42586dc7e235d5eca1f430d08d96836f4e61124
zhangyang <969666310@qq.com>
2020-01-09 11:36:44 +0800

    synchronized  synchronized 两个可重用锁的解析

---
+++ mianshi/src/main/java/cn/myfreecloud/java/lock/ReentrantLockDemo01.java
@@ -0,0 +1,57 @@
+package cn.myfreecloud.java.lock;
+
+/**
+ *  本案例说明 synchronized 是一个典型的可重用锁
+ */
+
+// 手机类
+class Phone {
+    public synchronized void sendSMS() throws Exception{
+        System.out.println(Thread.currentThread().getId() + "\t invoked sendSMS()");
+        sendEmail();
+    }
+
+    public synchronized void sendEmail() throws Exception{
+        System.out.println(Thread.currentThread().getId() + "\t *****************invoked sendEmail()");
+    }
+}
+
+
+// ReentrantLock 可重用锁(又名递归锁) 默认是非公平的锁,参数写true是公平的锁
+//synchronized 也是非公平锁
+
+// 可重用锁(又名递归锁)
+// 指的是同一个线程外层函数获得锁之后,内层递归函数仍然能够获取该锁的代码,在同一个线程在外层方法获取锁的时候,在进入内层方法会自动获取锁
+// 也就是说,线程可以进入任何一个它已经拥有的锁所同步着的代码块
+// ReentrantLock  和 synchronized 是典型的可重入锁(也叫递归锁),优势就是能够避免死锁
+//
+public class ReentrantLockDemo01 {
+
+    public static void main(String[] args) {
+
+        //13	 invoked sendSMS()
+        //13	 *****************invoked sendEmail()
+        //14	 invoked sendSMS()
+        //14	 *****************invoked sendEmail()
+        Phone phone = new Phone();
+        new Thread(
+                () -> {
+                    try {
+                        phone.sendSMS();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }, "t1").start();
+
+
+        new Thread(
+                () -> {
+                    try {
+                        phone.sendSMS();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }, "t1").start();
+    }
+
+}
---
+++ mianshi/src/main/java/cn/myfreecloud/java/lock/ReentrantLockDemo02.java
@@ -0,0 +1,101 @@
+package cn.myfreecloud.java.lock;
+
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 本案例说明 ReentrantLock 是一个典型的可重用锁
+ */
+
+class ReentrantPhone implements Runnable {
+    public synchronized void sendSMS() throws Exception {
+        System.out.println(Thread.currentThread().getId() + "\t invoked sendSMS()");
+        sendEmail();
+    }
+
+    public synchronized void sendEmail() throws Exception {
+        System.out.println(Thread.currentThread().getId() + "\t *****************invoked sendEmail()");
+    }
+
+    /**
+     * -------------------------------------
+     **/
+    Lock lock = new ReentrantLock();
+
+    @Override
+    public void run() {
+        get();
+    }
+
+    private void get() {
+        // 注意,加锁的次数和解锁的次数一定要匹配,否则会导致解锁失败,程序卡在这里,不会结束
+        // lock.lock();
+        lock.lock();
+        try {
+            System.out.println(Thread.currentThread().getId() + "\t invoked get()");
+            set();
+        } finally {
+            // lock.unlock();
+            lock.unlock();
+        }
+    }
+
+    private void set() {
+        lock.lock();
+        try {
+            System.out.println(Thread.currentThread().getId() + "\t *************** invoked set()");
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+
+public class ReentrantLockDemo02 {
+
+    public static void main(String[] args) {
+
+        //13	 invoked sendSMS()
+        //13	 *****************invoked sendEmail()
+        //14	 invoked sendSMS()
+        //14	 *****************invoked sendEmail()
+        ReentrantPhone reentrantphone = new ReentrantPhone();
+        new Thread(
+                () -> {
+                    try {
+                        reentrantphone.sendSMS();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }, "t1").start();
+
+
+        new Thread(
+                () -> {
+                    try {
+                        reentrantphone.sendSMS();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }, "t1").start();
+
+
+        try {
+            // 线程等待1s
+            TimeUnit.SECONDS.sleep(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("-----------------------------------------");
+        Thread t3 = new Thread(reentrantphone);
+
+        Thread t4 = new Thread(reentrantphone);
+
+        t3.start();
+        t4.start();
+    }
+
+}
