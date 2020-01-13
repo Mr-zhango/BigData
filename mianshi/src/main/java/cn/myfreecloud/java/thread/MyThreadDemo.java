bbcbb19976de4170b9f3ada499408bb6415f3a64
zhangyang <969666310@qq.com>
2020-01-13 18:06:47 +0800

    线程池-一池一线程

--- mianshi/src/main/java/cn/myfreecloud/java/thread/MyThreadDemo.java
+++ mianshi/src/main/java/cn/myfreecloud/java/thread/MyThreadDemo.java
@@ -13,16 +13,20 @@ public class MyThreadDemo {
         // Array Arrays
         // Collection Collections
         // Executor Executors
-        // ExecutorService threadPool = Executors.newFixedThreadPool(5); // 一池5个线程
-        ExecutorService threadPool = Executors.newSingleThreadExecutor(); // 一池1个线程
+        // ExecutorService threadPool = Executors.newFixedThreadPool(5); // 一池5个线程          执行长期的任务,性能比较高
+        // ExecutorService threadPool = Executors.newSingleThreadExecutor(); // 一池1个线程      一个任务一个任务执行
+        ExecutorService threadPool = Executors.newCachedThreadPool(); // 缓冲线程池              执行很多短期的异步任务
 
         // 模拟10个用户来连接,每个用户就是一个外部的请求线程
 
         try {
-            for (int i = 1; i <=20 ; i++) {
+            for (int i = 1; i <=10 ; i++) {
                 threadPool.execute(()->{
                     System.out.println(Thread.currentThread().getName() + "\t 办理业务");
                 });
+
+                // 暂停200ms
+                // try {TimeUnit.MILLISECONDS.sleep(200);} catch (Exception e) {e.printStackTrace();}
             }
         } catch (Exception e) {
             e.printStackTrace();
