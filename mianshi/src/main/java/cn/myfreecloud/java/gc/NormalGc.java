f0e66d000bab1c0892e4d3bdd52c56b654a8a627
zhangyang <969666310@qq.com>
2020-01-14 14:42:24 +0800

    gc垃圾回收机制

---
+++ mianshi/src/main/java/cn/myfreecloud/java/gc/NormalGc.java
@@ -0,0 +1,19 @@
+package cn.myfreecloud.java.gc;
+
+
+/**
+ * 常见的垃圾回收算法
+ *      引用计数
+ *      复制
+ *      标记清理
+ *      标记整理
+ *
+ *      为了解决引用计数法的循环引用问题,java使用了可达性分析的方法 gcroot
+ *
+ */
+
+public class NormalGc {
+    public static void main(String[] args) {
+
+    }
+}
