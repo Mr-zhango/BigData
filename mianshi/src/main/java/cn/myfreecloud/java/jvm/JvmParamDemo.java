e7646d056c5b39a74120f70e5bedd5d3fd970517
zhangyang <969666310@qq.com>
2020-01-23 22:13:18 +0800

    jvm常用参数设置

--- mianshi/src/main/java/cn/myfreecloud/java/jvm/JvmParamDemo.java
+++ mianshi/src/main/java/cn/myfreecloud/java/jvm/JvmParamDemo.java
@@ -30,14 +30,23 @@ package cn.myfreecloud.java.jvm;
  *
  *      jvm常用参数配置
  *      -Xms128m
+ *          最小堆，jvm运行的默认堆大小(物理内存的1/64)
  *      -Xmx4096m
+ *          指定最大堆，即堆内存的上线，当实际内存接近上线时会发生GC。(物理内存的1/4) 等价于 -XX:MaxHeapSize
  *      -Xss1024K
+ *          为jvm启动的每个线程分配的内存大小 等价于-XX:ThreadStackSize(默认128k-512k)
+ *      -Xmn
+ *          设置新生代的大小
  *      -XX:MetaSpaceSize=512M
+ *          元数据空间大小
  *      -XX:+PrintCommandLineFlags
+ *
  *      -XX:+PrintGCDetails
+ *          打印GC信息
  *      -XX:+UseSerialGC
  *
- *
+ *  常用参数设置
+ *  -Xms128m -Xmx4096m -Xss1024k -XX:MetaSpaceSize=512m -XX:+PrintCommandLineFlags -XX:+PrintGCDetails -XX:+UseSerialGC
  *
  *
  *
---
+++ mianshi/src/main/java/cn/myfreecloud/java/leetcode/ASumTest002.java
@@ -0,0 +1,8 @@
+package cn.myfreecloud.java.leetcode;
+
+public class ASumTest002 {
+
+    public static void main(String[] args) {
+
+    }
+}
