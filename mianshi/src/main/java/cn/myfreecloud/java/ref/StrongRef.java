d1799fa7a4ea24dde94949673045ac744b839bfa
zhangyang <969666310@qq.com>
2020-02-09 12:26:51 +0800

    强引用demo

--- mianshi/src/main/java/cn/myfreecloud/java/ref/StrongRef.java
+++ mianshi/src/main/java/cn/myfreecloud/java/ref/StrongRef.java
@@ -2,7 +2,9 @@ package cn.myfreecloud.java.ref;
 
 public class StrongRef {
     public static void main(String[] args) {
+        // 创建新的对象
         Object object1 = new Object();
+        // obj2强引用 object1 ,垃圾回收之后obj2不会被回收
         Object obj2 = object1;
         object1 = null;
         System.gc();
